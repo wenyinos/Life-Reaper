bf6bbdfcbb72c0bcdfcbaac6250b89fb927ccfd7
Wenyin Root <ruojiner@hotmail.com>
2026-04-05 21:25:47 +0800

    Add custom title bar with minimize/close buttons and translate all text to Chinese

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -18,8 +18,10 @@ namespace Life_Reaper
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
-            panelHeader = new Panel();
+            panelTitleBar = new Panel();
             lblTitle = new Label();
+            btnMinimize = new Button();
+            btnClose = new Button();
             panelContent = new Panel();
             lblWarning = new Label();
             lblDescription = new Label();
@@ -33,30 +35,65 @@ namespace Life_Reaper
             btnCheckPayment = new Button();
             lblTimer = new Label();
             timerCountdown = new System.Windows.Forms.Timer(components);
-            panelHeader.SuspendLayout();
+            panelTitleBar.SuspendLayout();
             panelContent.SuspendLayout();
             SuspendLayout();
             // 
-            // panelHeader
+            // panelTitleBar
             // 
-            panelHeader.BackColor = Color.FromArgb(192, 0, 0);
-            panelHeader.Controls.Add(lblTitle);
-            panelHeader.Dock = DockStyle.Top;
-            panelHeader.Location = new Point(0, 0);
-            panelHeader.Name = "panelHeader";
-            panelHeader.Size = new Size(750, 60);
-            panelHeader.TabIndex = 0;
+            panelTitleBar.BackColor = Color.FromArgb(192, 0, 0);
+            panelTitleBar.Controls.Add(btnClose);
+            panelTitleBar.Controls.Add(btnMinimize);
+            panelTitleBar.Controls.Add(lblTitle);
+            panelTitleBar.Dock = DockStyle.Top;
+            panelTitleBar.Location = new Point(0, 0);
+            panelTitleBar.Name = "panelTitleBar";
+            panelTitleBar.Size = new Size(780, 40);
+            panelTitleBar.TabIndex = 0;
+            panelTitleBar.MouseDown += PanelTitleBar_MouseDown;
             // 
             // lblTitle
             // 
             lblTitle.AutoSize = true;
-            lblTitle.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            lblTitle.Font = new Font("Microsoft YaHei UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
             lblTitle.ForeColor = Color.White;
-            lblTitle.Location = new Point(12, 9);
+            lblTitle.Location = new Point(12, 6);
             lblTitle.Name = "lblTitle";
-            lblTitle.Size = new Size(380, 45);
+            lblTitle.Size = new Size(200, 26);
             lblTitle.TabIndex = 0;
             lblTitle.Text = "续命 1926.08.17";
+            // 
+            // btnMinimize
+            // 
+            btnMinimize.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnMinimize.BackColor = Color.FromArgb(192, 0, 0);
+            btnMinimize.FlatAppearance.BorderSize = 0;
+            btnMinimize.FlatStyle = FlatStyle.Flat;
+            btnMinimize.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            btnMinimize.ForeColor = Color.White;
+            btnMinimize.Location = new Point(698, 4);
+            btnMinimize.Name = "btnMinimize";
+            btnMinimize.Size = new Size(36, 32);
+            btnMinimize.TabIndex = 1;
+            btnMinimize.Text = "─";
+            btnMinimize.UseVisualStyleBackColor = false;
+            btnMinimize.Click += BtnMinimize_Click;
+            // 
+            // btnClose
+            // 
+            btnClose.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnClose.BackColor = Color.FromArgb(192, 0, 0);
+            btnClose.FlatAppearance.BorderSize = 0;
+            btnClose.FlatStyle = FlatStyle.Flat;
+            btnClose.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            btnClose.ForeColor = Color.White;
+            btnClose.Location = new Point(740, 4);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(36, 32);
+            btnClose.TabIndex = 2;
+            btnClose.Text = "✕";
+            btnClose.UseVisualStyleBackColor = false;
+            btnClose.Click += BtnClose_Click;
             // 
             // panelContent
             // 
@@ -71,85 +108,86 @@ namespace Life_Reaper
             panelContent.Controls.Add(btnDecrypt);
             panelContent.Controls.Add(btnPay);
             panelContent.Controls.Add(btnCheckPayment);
+            panelContent.Controls.Add(lblTimer);
             panelContent.Dock = DockStyle.Fill;
-            panelContent.Location = new Point(0, 60);
+            panelContent.Location = new Point(0, 40);
             panelContent.Name = "panelContent";
-            panelContent.Size = new Size(750, 540);
+            panelContent.Size = new Size(780, 520);
             panelContent.TabIndex = 1;
             // 
             // lblWarning
             // 
             lblWarning.AutoSize = true;
-            lblWarning.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            lblWarning.Font = new Font("Microsoft YaHei UI", 20F, FontStyle.Bold, GraphicsUnit.Point);
             lblWarning.ForeColor = Color.FromArgb(192, 0, 0);
-            lblWarning.Location = new Point(25, 20);
+            lblWarning.Location = new Point(30, 20);
             lblWarning.Name = "lblWarning";
-            lblWarning.Size = new Size(520, 32);
+            lblWarning.Size = new Size(440, 36);
             lblWarning.TabIndex = 0;
-            lblWarning.Text = "Ooops, your files have been encrypted!";
+            lblWarning.Text = "哎呀，你的文件已被加密！";
             // 
             // lblDescription
             // 
-            lblDescription.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            lblDescription.Font = new Font("Microsoft YaHei UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
             lblDescription.ForeColor = Color.FromArgb(50, 50, 50);
-            lblDescription.Location = new Point(25, 65);
+            lblDescription.Location = new Point(30, 70);
             lblDescription.Name = "lblDescription";
-            lblDescription.Size = new Size(700, 100);
+            lblDescription.Size = new Size(720, 110);
             lblDescription.TabIndex = 1;
-            lblDescription.Text = "What happened to your files?\nAll of your files were protected by a strong encryption. Your files are safe, but you cannot access them until you pay.\n\nWhat guarantees do we give?\nWe care about our reputation and we guarantee decryption. You can decrypt one file for free to test.";
+            lblDescription.Text = "你的文件怎么了？\n所有文件都已被强加密保护。文件是安全的，但在付款前你无法访问。\n\n我们有什么保证？\n我们注重信誉，保证可以解密。你可以免费解密一个文件进行测试。";
             // 
             // lblCountdownLabel
             // 
             lblCountdownLabel.AutoSize = true;
-            lblCountdownLabel.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            lblCountdownLabel.Font = new Font("Microsoft YaHei UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
             lblCountdownLabel.ForeColor = Color.FromArgb(192, 0, 0);
-            lblCountdownLabel.Location = new Point(25, 180);
+            lblCountdownLabel.Location = new Point(30, 200);
             lblCountdownLabel.Name = "lblCountdownLabel";
-            lblCountdownLabel.Size = new Size(280, 25);
+            lblCountdownLabel.Size = new Size(200, 26);
             lblCountdownLabel.TabIndex = 2;
-            lblCountdownLabel.Text = "Payment will be raised on:";
+            lblCountdownLabel.Text = "价格将在以下时间后上涨：";
             // 
             // lblCountdown
             // 
             lblCountdown.AutoSize = true;
-            lblCountdown.Font = new Font("Segoe UI", 28F, FontStyle.Bold, GraphicsUnit.Point);
+            lblCountdown.Font = new Font("Consolas", 32F, FontStyle.Bold, GraphicsUnit.Point);
             lblCountdown.ForeColor = Color.FromArgb(192, 0, 0);
-            lblCountdown.Location = new Point(25, 210);
+            lblCountdown.Location = new Point(30, 235);
             lblCountdown.Name = "lblCountdown";
-            lblCountdown.Size = new Size(180, 50);
+            lblCountdown.Size = new Size(210, 51);
             lblCountdown.TabIndex = 3;
             lblCountdown.Text = "07:00:00";
             // 
             // lblInstructions
             // 
             lblInstructions.AutoSize = true;
-            lblInstructions.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblInstructions.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
             lblInstructions.ForeColor = Color.FromArgb(50, 50, 50);
-            lblInstructions.Location = new Point(25, 280);
+            lblInstructions.Location = new Point(30, 305);
             lblInstructions.Name = "lblInstructions";
-            lblInstructions.Size = new Size(180, 21);
+            lblInstructions.Size = new Size(160, 22);
             lblInstructions.TabIndex = 4;
-            lblInstructions.Text = "Send exact amount to:";
+            lblInstructions.Text = "请发送精确金额到：";
             // 
             // lblBtcAddress
             // 
             lblBtcAddress.AutoSize = true;
             lblBtcAddress.Font = new Font("Consolas", 12F, FontStyle.Regular, GraphicsUnit.Point);
             lblBtcAddress.ForeColor = Color.FromArgb(0, 100, 200);
-            lblBtcAddress.Location = new Point(25, 310);
+            lblBtcAddress.Location = new Point(30, 335);
             lblBtcAddress.Name = "lblBtcAddress";
-            lblBtcAddress.Size = new Size(420, 19);
+            lblBtcAddress.Size = new Size(360, 19);
             lblBtcAddress.TabIndex = 5;
             lblBtcAddress.Text = "1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa";
             // 
             // lblBtcAmount
             // 
             lblBtcAmount.AutoSize = true;
-            lblBtcAmount.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            lblBtcAmount.Font = new Font("Microsoft YaHei UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
             lblBtcAmount.ForeColor = Color.FromArgb(192, 0, 0);
-            lblBtcAmount.Location = new Point(25, 345);
+            lblBtcAmount.Location = new Point(30, 370);
             lblBtcAmount.Name = "lblBtcAmount";
-            lblBtcAmount.Size = new Size(150, 25);
+            lblBtcAmount.Size = new Size(110, 26);
             lblBtcAmount.TabIndex = 6;
             lblBtcAmount.Text = "0.05 BTC";
             // 
@@ -158,13 +196,13 @@ namespace Life_Reaper
             btnDecrypt.BackColor = Color.FromArgb(192, 0, 0);
             btnDecrypt.FlatAppearance.BorderSize = 0;
             btnDecrypt.FlatStyle = FlatStyle.Flat;
-            btnDecrypt.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnDecrypt.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
             btnDecrypt.ForeColor = Color.White;
-            btnDecrypt.Location = new Point(25, 400);
+            btnDecrypt.Location = new Point(30, 420);
             btnDecrypt.Name = "btnDecrypt";
             btnDecrypt.Size = new Size(200, 45);
             btnDecrypt.TabIndex = 7;
-            btnDecrypt.Text = "DECRYPT FILES";
+            btnDecrypt.Text = "解密文件";
             btnDecrypt.UseVisualStyleBackColor = false;
             btnDecrypt.Click += BtnDecrypt_Click;
             // 
@@ -173,13 +211,13 @@ namespace Life_Reaper
             btnPay.BackColor = Color.FromArgb(0, 100, 200);
             btnPay.FlatAppearance.BorderSize = 0;
             btnPay.FlatStyle = FlatStyle.Flat;
-            btnPay.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnPay.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
             btnPay.ForeColor = Color.White;
-            btnPay.Location = new Point(250, 400);
+            btnPay.Location = new Point(260, 420);
             btnPay.Name = "btnPay";
             btnPay.Size = new Size(200, 45);
             btnPay.TabIndex = 8;
-            btnPay.Text = "PAY BITCOIN";
+            btnPay.Text = "支付比特币";
             btnPay.UseVisualStyleBackColor = false;
             btnPay.Click += BtnPay_Click;
             // 
@@ -188,26 +226,26 @@ namespace Life_Reaper
             btnCheckPayment.BackColor = Color.FromArgb(80, 80, 80);
             btnCheckPayment.FlatAppearance.BorderSize = 0;
             btnCheckPayment.FlatStyle = FlatStyle.Flat;
-            btnCheckPayment.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnCheckPayment.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
             btnCheckPayment.ForeColor = Color.White;
-            btnCheckPayment.Location = new Point(475, 400);
+            btnCheckPayment.Location = new Point(490, 420);
             btnCheckPayment.Name = "btnCheckPayment";
             btnCheckPayment.Size = new Size(200, 45);
             btnCheckPayment.TabIndex = 9;
-            btnCheckPayment.Text = "CHECK PAYMENT";
+            btnCheckPayment.Text = "检查付款";
             btnCheckPayment.UseVisualStyleBackColor = false;
             btnCheckPayment.Click += BtnCheckPayment_Click;
             // 
             // lblTimer
             // 
             lblTimer.AutoSize = true;
-            lblTimer.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            lblTimer.Font = new Font("Microsoft YaHei UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
             lblTimer.ForeColor = Color.FromArgb(100, 100, 100);
-            lblTimer.Location = new Point(25, 465);
+            lblTimer.Location = new Point(30, 485);
             lblTimer.Name = "lblTimer";
-            lblTimer.Size = new Size(350, 19);
+            lblTimer.Size = new Size(280, 19);
             lblTimer.TabIndex = 10;
-            lblTimer.Text = "Time left to decrypt for free: ";
+            lblTimer.Text = "免费解密剩余时间：";
             // 
             // timerCountdown
             // 
@@ -219,16 +257,16 @@ namespace Life_Reaper
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(750, 600);
+            ClientSize = new Size(780, 560);
             Controls.Add(panelContent);
-            Controls.Add(panelHeader);
+            Controls.Add(panelTitleBar);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "续命 1926.08.17";
             TopMost = true;
-            panelHeader.ResumeLayout(false);
-            panelHeader.PerformLayout();
+            panelTitleBar.ResumeLayout(false);
+            panelTitleBar.PerformLayout();
             panelContent.ResumeLayout(false);
             panelContent.PerformLayout();
             ResumeLayout(false);
@@ -236,8 +274,10 @@ namespace Life_Reaper
 
         #endregion
 
-        private Panel panelHeader;
+        private Panel panelTitleBar;
         private Label lblTitle;
+        private Button btnMinimize;
+        private Button btnClose;
         private Panel panelContent;
         private Label lblWarning;
         private Label lblDescription;
--- Form1.cs
+++ Form1.cs
@@ -1,8 +1,11 @@
+using System.Drawing.Drawing2D;
+
 namespace Life_Reaper
 {
     public partial class Form1 : Form
     {
         private TimeSpan timeLeft;
+        private Point mouseOffset;
 
         public Form1()
         {
@@ -22,29 +25,55 @@ namespace Life_Reaper
             {
                 timerCountdown.Stop();
                 lblCountdown.Text = "00:00:00";
-                MessageBox.Show("Time expired! The price has been doubled.", "Time Expired", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                MessageBox.Show("时间已到！价格已翻倍。", "时间到期", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
 
         private void UpdateCountdownDisplay()
         {
             lblCountdown.Text = timeLeft.ToString(@"hh\:mm\:ss");
-            lblTimer.Text = $"Time left to decrypt for free: {timeLeft.ToString(@"hh\:mm\:ss")}";
+            lblTimer.Text = $"免费解密剩余时间：{timeLeft.ToString(@"hh\:mm\:ss")}";
         }
 
         private void BtnDecrypt_Click(object? sender, EventArgs e)
         {
-            MessageBox.Show("Payment not detected. Please send the required amount first.", "Decryption Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            MessageBox.Show("未检测到付款。请先发送所需金额。", "解密失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
         private void BtnPay_Click(object? sender, EventArgs e)
         {
-            MessageBox.Show("Send 0.05 BTC to the address above.\nAfter payment, click 'Check Payment' to verify.", "Payment Instructions", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            MessageBox.Show("请发送 0.05 BTC 到上方地址。\n付款后，点击\"检查付款\"进行验证。", "付款说明", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         private void BtnCheckPayment_Click(object? sender, EventArgs e)
         {
-            MessageBox.Show("No payment detected yet. Please wait for blockchain confirmation.", "Checking Payment", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            MessageBox.Show("尚未检测到付款。请等待区块链确认。", "检查付款", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void BtnMinimize_Click(object? sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void BtnClose_Click(object? sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void PanelTitleBar_MouseDown(object? sender, MouseEventArgs e)
+        {
+            mouseOffset = new Point(-e.X, -e.Y);
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            if (e.Button == MouseButtons.Left)
+            {
+                Point mousePos = Control.MousePosition;
+                mousePos.Offset(mouseOffset.X, mouseOffset.Y);
+                Location = mousePos;
+            }
         }
     }
 }
